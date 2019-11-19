f8e689ba43336b72c83da115991a488de349c441
catmorte <catmorte@gmail.com>
2019-11-19 20:42:30 +0300

    Create chain.js

---
+++ chain.js
@@ -0,0 +1,41 @@
+function chain(data) {
+  var instance = function instance(element){
+    var newChain = instance.functions.slice();
+    if (typeof element === "function") {
+      newChain.push(element)
+    }
+    var newStream = chain(data);
+    newStream.functions = newChain;
+    return newStream;
+  };
+  instance.functions = [];
+  instance.data = data;
+  instance.clank = function(){
+    var localData = instance.data.slice();
+    chainsLoop: for(var i in instance.functions) {
+      var newLocalData = [];
+      var broken = false;
+      var brokenValue = undefined;
+      dataLoop: for(var j in localData) {
+        var result = instance.functions[i](localData[j], j);
+        if(result.break) {
+          localData = newLocalData;
+          broken = true;
+          brokenValue = result.data;
+          break chainsLoop;
+        }
+        if(result.stop) {
+          localData = newLocalData;
+          continue chainsLoop;
+        }
+        if(result.skip){
+          continue dataLoop;
+        }
+        newLocalData.push(result.data)
+      }
+      localData = newLocalData;
+    }
+    return {data:localData, broken: broken, brokenValue:brokenValue};
+  }
+  return instance;
+}
